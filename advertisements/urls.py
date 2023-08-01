d36c28b904c3005eb73584f2831b2ef9f326e00b
RasimKuchchaev <rasimkuchchaev@gmail.com>
2023-08-01 10:02:47 +0300

    2.4

--- advertisements/urls.py
+++ advertisements/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.advertisement_list, name='advertisement_list'),
-    path("advertisement", views.advertisement_detail)
+    path("", views.advertisement_list, name='advertisement_list')
 ]
