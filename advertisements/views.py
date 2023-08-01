54b28468be8265518e0b3287386597eea14de3ec
RasimKuchchaev <rasimkuchchaev@gmail.com>
2023-08-01 13:33:39 +0300

    3.4 language shablone

--- advertisements/views.py
+++ advertisements/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 
 def advertisement_list(request, *args, **kwargs):
-    return render(request, 'advertisement/advertisements_list.html')
+    advertisements = [
+        'Мастер на час',
+        'Ремонт компьютера',
+        'Услуги сантехника'
+    ]
+    return render(request, 'advertisement/advertisements_list.html', {'advertisements':advertisements})
 
 
