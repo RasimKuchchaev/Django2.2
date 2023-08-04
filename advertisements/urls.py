2a33c4dd5dbd3bdfacaab85e160f3c91ec7431aa
RasimKuchchaev <rasimkuchchaev@gmail.com>
2023-08-04 11:31:37 +0300

    3.6 Generic View

--- advertisements/urls.py
+++ advertisements/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
--- advertisements/views.py
+++ advertisements/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
 
 def advertisement_list(request, *args, **kwargs):
     advertisements = [
@@ -10,10 +12,22 @@ def advertisement_list(request, *args, **kwargs):
     ]
     return render(request, 'advertisement/advertisements_list.html', {'advertisements': advertisements})
 
-class About(View):
-    def get(self, request):
-        return render(request, 'advertisement/about.html',{'about':'about-qwerty'})
+class About(TemplateView):
+    template_name = 'advertisement/about.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Бесплатные обьявления в вашем городе'
+        context['title'] = 'Бесплатные обьявления'
+        context['description'] = """
+            Господа, граница обучения кадров прекрасно подходит для реализации новых принципов формирования 
+            материально-технической и кадровой базы. В частности, постоянный количественный рост и сфера нашей 
+            активности позволяет оценить значение как самодостаточных, так и внешне зависимых концептуальных решений. 
+            Однозначно, тщательные исследования конкурентов описаны максимально подробно. Безусловно, существующая 
+            теория предопределяет высокую востребованность системы массового участия. Господа, социально-экономическое 
+            развитие предполагает независимые способы реализации существующих финансовых и административных условий.
+        """
+        return context
 
 
 def contact_list(request, *args, **kwargs):
