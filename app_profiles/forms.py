2665e3b0e51ca7566cc2d39b9a226d7b6b1699d5
RasimKuchchaev <rasimkuchchaev@gmail.com>
2023-08-14 12:52:39 +0300

    5.5 Дополнительные возможности валидации форм

--- app_profiles/forms.py
+++ app_profiles/forms.py
@@ -31,7 +31,7 @@ class UserForm(forms.Form):
 
 
     """
-    PS C:\Users\Rasim\PycharmProjects\board> python manage.py shell
+    python manage.py shell
     Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
     Type "help", "copyright", "credits" or "license" for more information.
     (InteractiveConsole)
