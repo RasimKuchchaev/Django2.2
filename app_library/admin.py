e398472d3a728577fb7aaa529d86279a7750ea33
RasimKuchchaev <rasimkuchchaev@gmail.com>
2023-08-18 11:52:55 +0300

    6.4 Редактирование связанных записей

--- app_library/admin.py
+++ app_library/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
-from app_library.models import Publisher, Author
+from app_library.models import Publisher, Author, Book
 
+
+# class BookInline(admin.TabularInline):
+class BookInline(admin.StackedInline):
+    model = Book
 
 class PublisherAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'city']
     list_filter = ['is_active', 'city']
+    inlines = [BookInline]
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name']
     search_fields = ['first_name', 'last_name', 'biography']
 
 
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Publisher, PublisherAdmin)
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
 
--- app_library/models.py
+++ app_library/models.py
@@ -8,10 +8,24 @@ class Publisher(models.Model):
     country = models.CharField(max_length=50)
     is_active = models.BooleanField()
 
+    def __str__(self):
+        return f"{self.id}. {self.name} ({self.country}, {self.city})"
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    emal = models.EmailField()
+    email = models.EmailField()
     biography = models.TextField()
+
+    def __str__(self):
+        return f"{self.id}. {self.first_name}, {self.last_name}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+
 
