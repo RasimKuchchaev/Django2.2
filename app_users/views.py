63d13b8d3b9d6a4e6cf5edeb0eee6fd1bc94686c
RasimKuchchaev <rasimkuchchaev@gmail.com>
2023-08-21 11:49:46 +0300

    7.2 Пишем view для аутентификации

---
+++ app_users/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render
+from app_users.forms import AuthForm
+
+
+def login_view(request):
+    if request.method == 'POST':
+        auth_form = AuthForm(request.POST)
+        if auth_form.is_valid():
+            username = auth_form.cleaned_data['username']
+            password = auth_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Вы успешно вошли в систему")
+                else:
+                    auth_form.add_error('__all__', 'Учетная запись пользователя не активна!')
+            else:
+                auth_form.add_error('__all__', 'Ошибка! Проверте правильность написания логина и пароля')
+    else:
+        auth_form = AuthForm()
+        context = {
+            'form': auth_form
+        }
+        return render(request, 'users/login.html', context=context)
+
