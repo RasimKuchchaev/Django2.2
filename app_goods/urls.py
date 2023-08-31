28c3a79b554cabdc1bef2017750e92ca2d97aa29
RasimKuchchaev <rasimkuchchaev@gmail.com>
2023-08-31 13:37:34 +0300

    9.2 Обработка загруженных файлов

---
+++ app_goods/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from app_goods.views import items_list, update_prices
+
+urlpatterns = [
+    path('items/', items_list, name='item_list'),
+    path('update_prices/', update_prices, name='update_prices'),
+]
